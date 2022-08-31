cfeadb79d3e3fda57eac5a82d79720f854c2cb1a
mayorathan <mayorathan@outlook.com>
2022-08-31 12:04:18 -0400

    Fixed contact bar

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,11 +4,11 @@ import { CONTACT } from "../content/about";
 
 const Contact = () => {
   return (
-    <div className="flex mt-10 mb-6 text-cloud  bg-background md:mb-14 lg:place-self-end">
+    <div className="flex mt-10 mb-6 text-cloud  bg-background md:mb-14 md:place-self-end">
       <nav>
         {CONTACT.map(provider => (
           <a
-            className="inline-block px-2"
+            className="inline-block px-2 md:px-4 lg:px-6"
             href={provider.link}
             target="_blank"
             rel="noreferrer">
