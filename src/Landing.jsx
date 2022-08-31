62a2a91547882a9c64cd2194e4ba3d8fef553a96
mayorathan <mayorathan@outlook.com>
2022-08-31 12:36:42 -0400

    Added background gradient

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -3,7 +3,6 @@ import Hero from "./components/Hero/Hero";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 
-// max-w-screen-lg
 const Landing = () => (
   <div className="flex flex-col items-center space-y-2 md:max-w-screen-lg md:pl-10 md:pr-10 lg:mx-auto">
     <Contact />
